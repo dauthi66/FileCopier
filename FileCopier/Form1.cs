5d95983b858a80bef19ac24db975d8846316bcac
Kris <dauthi66@gmail.com>
2022-03-28 15:05:19 -0700

    Refactored two new methods, ParseToDateTime() and FormatPathToDate(). Moved filePaths[] to main. Started on new method CopyFile();

--- FileCopier/Form1.cs
+++ FileCopier/Form1.cs
@@ -11,70 +11,82 @@ namespace FileCopier
 
         private void btnCopyFile_Click(object sender, EventArgs e)
         {
+            //TODO: Test all exceptions
+
             //find file location
             string userDirectory = "C:/Test Folder";
 
+            string[] filePaths = Directory.GetFiles(userDirectory);
+
             if (Directory.Exists(userDirectory))
             {
-                DateTime newestDate = FileNamesToDateTimes(userDirectory);
+                string newestDate = FindNewestFile(filePaths);
+                CopyFile(filePaths);
             }
         }
 
-        private static DateTime FileNamesToDateTimes(string userDirectory)
+        private static string FindNewestFile(string[] filePaths)
         {
-            string[] filePaths = Directory.GetFiles(userDirectory);
+            //Create array of files from directory
 
             DateTime nextDate = new();
 
-            //fencepost first date
-            string filePath = filePaths[0];
-            //format string
-            int indexOfName = filePath.IndexOf("_") + 1;
-            string dateOnly = filePath.Substring(indexOfName, 8);
-            //convert to DateTime
-            DateTime newestDate = DateTime.ParseExact(dateOnly, "yyyyMMdd", new CultureInfo("en-US"));
+            //Fence post: format string and convert to DateTime. 
+            string newestDateString = FormatPathToDate(filePaths[0]);
+            DateTime newestDate = ParseToDateTime(newestDateString);
 
-            //start from 1 to avoid fencepost
             for (int i = 1; i < filePaths.Length; i++)
             {
                 if (!filePaths.Any())
                 {
                     throw new NullReferenceException("There are no files in that folder");
                 }
-                
-                filePath = filePaths[i];
 
-                if (filePath.Contains("_"))
+                //create next date for comparison
+                nextDate = ParseToDateTime(filePaths[i]);
+
+                //compare the dates, keep the newest
+                if (newestDate.CompareTo(nextDate) < 0)
                 {   
-                    //create string of only dates
-                    indexOfName = filePath.IndexOf("_") + 1;
-                    dateOnly = filePath.Substring(indexOfName, 8);
-
-                    //Convert string to DateTime and add to list
-                    try
-                    {
-                        nextDate = DateTime.ParseExact(dateOnly, "yyyyMMdd", new CultureInfo("en-US"));
-                    }
-                    catch (FormatException)
-                    {
-                        throw new FormatException($"The file {filePath} is not labeled in the correct yyyyMMdd format");
-                    }
-
-                    if (newestDate.CompareTo(nextDate) < 0)
-                    {
-                        
-                        newestDate = nextDate;
-                    }
-                }
-                else
-                {
-                    throw new FormatException($"The file {filePath} is not using a _ in it's format");
+                    newestDateString = filePaths[i];
+                    newestDate = nextDate;
                 }
             }
 
-            return newestDate;
+            return newestDateString;
         }
 
+        private static DateTime ParseToDateTime(string dateOnly)
+        {          
+                try
+                {
+                    DateTime date = DateTime.ParseExact(dateOnly, "yyyyMMdd", new CultureInfo("en-US"));
+                    return date;
+                }
+                catch (FormatException)
+                {
+                    throw new FormatException($"The file with date {dateOnly} is not labeled in the correct yyyyMMdd format");
+                }
+        }
+        private static string FormatPathToDate(string pathToFormat)
+        {
+            if (pathToFormat.Contains("_"))
+            {
+                //create string of only dates
+                int indexOfName = pathToFormat.IndexOf("_") + 1;
+                string dateOnly = pathToFormat.Substring(indexOfName, 8);
 
+                return dateOnly;
+            }
+            else
+            {
+                throw new FormatException($"The file {pathToFormat} is not using a _ in it's format");
+            }
+        }
+
+        private static void CopyFile(string[] filePaths)
+        {
+
+        }
     }
 }
