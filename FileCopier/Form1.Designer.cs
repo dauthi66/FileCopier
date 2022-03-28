5c657f823e2b06071ec9fd7510c284e6aba313d9
Kris <dauthi66@gmail.com>
2022-03-28 12:48:40 -0700

    Add project files.

---
+++ FileCopier/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace FileCopier
+{
+    partial class FrmFileCopier
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCopyFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCopyFile
+            // 
+            this.btnCopyFile.Location = new System.Drawing.Point(135, 209);
+            this.btnCopyFile.Name = "btnCopyFile";
+            this.btnCopyFile.Size = new System.Drawing.Size(132, 46);
+            this.btnCopyFile.TabIndex = 0;
+            this.btnCopyFile.Text = "Copy File";
+            this.btnCopyFile.UseVisualStyleBackColor = true;
+            this.btnCopyFile.Click += new System.EventHandler(this.btnCopyFile_Click);
+            // 
+            // FrmFileCopier
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 372);
+            this.Controls.Add(this.btnCopyFile);
+            this.Name = "FrmFileCopier";
+            this.Text = "File Copier";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button btnCopyFile;
+    }
+}
---
+++ FileCopier/Form1.cs
@@ -0,0 +1,90 @@
+using System.Globalization;
+
+namespace FileCopier
+{
+    public partial class FrmFileCopier : Form
+    {
+        public FrmFileCopier()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCopyFile_Click(object sender, EventArgs e)
+        {
+            //find file location
+            string userDirectory = "C:/Test Folder";
+
+            if (Directory.Exists(userDirectory))
+            {
+                DateTime newestDate = FileNamesToDateTimes(userDirectory);
+            }
+
+            
+            //foreach (var date in dateTimes)
+            //{
+            //    MessageBox.Show(date.ToString());
+            //}
+
+
+            //sort order info : https://stackoverflow.com/questions/18070517/sort-datetime-list-by-time
+
+        }
+
+        private static DateTime FileNamesToDateTimes(string userDirectory)
+        {
+            string[] filePaths = Directory.GetFiles(userDirectory);
+
+            DateTime nextDate = new();
+
+            //fencepost first date
+            string filePath = filePaths[0];
+            //format string
+            int indexOfName = filePath.IndexOf("_") + 1;
+            string dateOnly = filePath.Substring(indexOfName, 8);
+            //convert to DateTime
+            DateTime newestDate = DateTime.ParseExact(dateOnly, "yyyyMMdd", new CultureInfo("en-US"));
+
+            //start from 1 to avoid fencepost
+            for (int i = 1; i < filePaths.Length; i++)
+            {
+                if (!filePaths.Any())
+                {
+                    throw new NullReferenceException("There are no files in that folder");
+                }
+                
+                filePath = filePaths[i];
+
+                if (filePath.Contains("_"))
+                {   
+                    //create string of only dates
+                    indexOfName = filePath.IndexOf("_") + 1;
+                    dateOnly = filePath.Substring(indexOfName, 8);
+
+                    //Convert string to DateTime and add to list
+                    try
+                    {
+                        nextDate = DateTime.ParseExact(dateOnly, "yyyyMMdd", new CultureInfo("en-US"));
+                    }
+                    catch (FormatException)
+                    {
+                        throw new FormatException($"The file {filePath} is not labeled in the correct yyyyMMdd format");
+                    }
+
+                    if (newestDate.CompareTo(nextDate) < 0)
+                    {
+                        
+                        newestDate = nextDate;
+                    }
+                }
+                else
+                {
+                    throw new FormatException($"The file {filePath} is not using a _ in it's format");
+                }
+            }
+
+            return newestDate;
+        }
+
+
+    }
+}
